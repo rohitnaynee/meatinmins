e7ab6d8052d7da1f4ef929b74a4c214c47db4c5f
rohit naynee <rohitnaynee@gmail.com>
2019-11-27 22:58:25 +0530

    adding header component and links to home with vector SVG

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ChickenPage from './pages/chickenpage/chickenpage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 const Mutton = () => (
   <div>
@@ -34,13 +35,16 @@ const Eggs = () => (
 function App() {
   return (
     <div>
-      <Route exact path = '/' component = {HomePage} />
-      <Route exact path ='/chicken'  component = {ChickenPage} />  
-      <Route exact path = '/shop' component = {ShopPage} /> 
-      <Route exact path = '/mutton' component = {Mutton} />
-      <Route exact path = '/seafood' component = {SeaFood} />
-      <Route exact path = '/exotic' component = {Exotic} />
-      <Route exact path = '/eggs' component = {Eggs} />
+      <Header />
+        <Switch>
+        <Route exact path = '/' component = {HomePage} />
+        <Route exact path ='/chicken'  component = {ChickenPage} />  
+        <Route exact path = '/shop' component = {ShopPage} /> 
+        <Route exact path = '/mutton' component = {Mutton} />
+        <Route exact path = '/seafood' component = {SeaFood} />
+        <Route exact path = '/exotic' component = {Exotic} />
+        <Route exact path = '/eggs' component = {Eggs} />
+        </Switch>    
     </div>
   );
 }
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { ReactComponent as Logo } from '../../assets/chicken.svg';
+
+import './header.styles.scss';
+
+const Header = () => (
+    <div className = 'header' >
+      <Link className = 'logo-container' to = "/" >
+        <Logo className = 'logo' />
+      </Link>
+      <div className = 'options' >
+        <Link className = 'option' to = '/shop' >
+           SHOP
+        </Link>
+
+        <Link className = 'option' to = '/shop' >
+           CONTACT
+        </Link>
+
+        <Link className = 'option' to = '/shop' >
+           ABOUT US 
+        </Link>
+
+        <Link className = 'option' to = '/shop' >
+           WE ARE HIRING
+        </Link>
+      </div>
+    </div>
+);
+
+export default Header;
